e04ae5855665026ab898d0d158a3ccf3d80c5841
Marissa Borges <marissaborges2006@gmail.com>
2025-06-09 15:12:12 -0300

    Adicionando verificações aos loaders de documentos

--- loaders.py
+++ loaders.py
@@ -1,31 +1,36 @@
 from langchain_community.document_loaders import WebBaseLoader, YoutubeLoader, CSVLoader, PyPDFLoader, TextLoader
 
 def carrega_site(url):
-    loader = WebBaseLoader(url)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    if not url == '':
+        loader = WebBaseLoader(url)
+        lista_documentos = loader.load()
+        documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+        return documento
 
 def carrega_youtube(video_id):
-    loader = YoutubeLoader(video_id, add_video_info=False, language=['pt'])
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    if not video_id == '':
+        loader = YoutubeLoader(video_id, add_video_info=False, language=['pt'])
+        lista_documentos = loader.load()
+        documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+        return documento
 
 def carrega_csv(caminho):
-    loader = CSVLoader(caminho)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    if not caminho == '':
+        loader = CSVLoader(caminho)
+        lista_documentos = loader.load()
+        documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+        return documento
 
 def carrega_pdf(caminho):
-    loader = PyPDFLoader(caminho)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    if not caminho == '':
+        loader = PyPDFLoader(caminho)
+        lista_documentos = loader.load()
+        documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+        return documento
 
 def carrega_txt(caminho):
-    loader = TextLoader(caminho)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    if not caminho == '':
+        loader = TextLoader(caminho)
+        lista_documentos = loader.load()
+        documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+        return documento
